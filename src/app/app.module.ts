fb8b956abe4876dcf0e2e3d56909d37868b8d719
Kristjan Vingel <kristjan.vingel.protonmail.ch>
2023-02-27 12:22:34 +0700

    Heading links added. Removed un-necessary imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,7 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { MatIconModule } from '@angular/material/icon';
-import { MatCardModule } from '@angular/material/card';
-import { MatTabsModule } from '@angular/material/tabs';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,8 +17,6 @@ import { SliderComponent } from './slider/slider.component';
     BrowserAnimationsModule,
     MatIconModule,
     HttpClientModule,
-    MatCardModule,
-    MatTabsModule,
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
